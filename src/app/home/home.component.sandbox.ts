4ffd6c29cc9e081f987d4685907d260297a1796c
Connie Leung <connie.leung@diginex.com>
2020-03-11 15:33:38 +0800

    fix: import NgxPermissionsModule.forRoot() to sandbox component in lazy-loaded module
    Signed-off-by: Connie Leung <connie.leung@diginex.com>

--- src/app/home/home.component.sandbox.ts
+++ src/app/home/home.component.sandbox.ts
@@ -1,16 +1,13 @@
 import { sandboxOf } from 'angular-playground';
-import { NgxPermissionsService } from 'ngx-permissions';
+import { NgxPermissionsModule } from 'ngx-permissions';
 import { HomeComponent } from './home.component';
 import { HomeModule } from './home.module';
 
 export default sandboxOf(HomeComponent, {
-  label: 'ngx-permissions',
-  declareComponent: false,
-  imports: [HomeModule],
-  providers: [NgxPermissionsService],
-  declarations: [],
-})
-  .add('Home Component', {
+    label: 'ngx-permissions',
+    declareComponent: false,
+    imports: [HomeModule, NgxPermissionsModule.forRoot()],
+}).add('Home Component', {
     template: `<app-home></app-home>`,
-    context: {}
-  });
+    context: {},
+});
